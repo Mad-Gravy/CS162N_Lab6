144041401cbe629a292ff1843635772c7abd9742
Joseph Teague <graveytea@gmail.com, teaguej@my.lanecc.edu>
2026-05-24 11:41:54 -0700

    BJHand class done, tests took awhile cus I realized I hadn't written all the tests for Hand.

--- BJHand.cs
+++ BJHand.cs
@@ -2,16 +2,59 @@
 
 namespace BlackJackClasses
 {
-    public class BJHand : Hand
+    public class BJHand:Hand
     {
-        public BJHand() : base()
+        public BJHand():base() // Default Constructor, called from the parent class (Hand)
         {
 
         }
 
-        public BJHand(Deck d, int numCards) : base(d, numCards)
+        public BJHand(Deck d, int numCards) : base(d, numCards) // Overloaded Constructor, called from the parent class
         {
 
+        }
+
+        public bool HasAce // Checks the hand for an Ace using HasCard() from the parent class
+        {
+            get
+            {
+                return HasCard(1);
+            }
+        }
+
+        public int Score // Calculates the score of the hand according to Blackjack rules
+        {
+            get
+            {
+                int score = 0;
+
+                foreach (Card c in cards)
+                {
+                    if (c.Value >= 11 && c.Value <= 13) // Face cards count as 10 points
+                    {
+                        score += 10;
+                    }
+                    else
+                    {
+                        score += c.Value; // Number cards count as their value, and Aces count as 1 point for now
+                    }
+                }
+
+                if (HasAce && score <= 11) // Boost ace from 1 to 10 points if it doesn't cause a bust
+                {
+                    score += 10;
+                }
+
+                return score;
+            }
+        }
+
+        public bool IsBusted // Sad...
+        {
+            get
+            {
+                return Score > 21; // You done goofed.
+            } 
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,24 +7,89 @@ namespace Lab_6_Inheritance
     {
         static void Main(string[] args)
         {
-            Hand h = new Hand(); // Creates a hand object
-            Card c = new Card(1, 1); // Creates an Ace of Clubs
-
-            h.AddCard(c); // Adds the card to the hand
-            h.AddCard(new Card(13, 4)); // Adds a King of Spades
-            h.AddCard(new Card(7, 2)); // Adds a 7 of Diamonds
-
-            Console.WriteLine("Showing hand. Expecting 3 cards. " + h); // Displays the hand, which should show 3 cards
-
-            Console.WriteLine("Testing NumCards(), number of cards in hand. Expecting 3: " + h.NumCards); // Displays the count of cards
-
-            Console.WriteLine("Testing HasCard(), checking for card with value 1. Expecting True: " + h.HasCard(1)); // Tests if the hand has a card with value 1 (Ace).
-
-            Card removed = h.Discard(0); //Removes and returns the first card (Ace of Clubs) from the hand and stores it in 'removed'
-
-            Console.WriteLine("Testing Discard(), discard and return card. Expecting Ace of Clubs: " + removed); // Displays discarded card
+            TestHand();
+            TestBJHand();
 
             Console.ReadLine();
+        }
+
+        static void TestHand()
+        {
+            Hand h = new Hand();
+
+            h.AddCard(new Card(1, 1)); // Ace of Clubs
+            h.AddCard(new Card(13, 4)); // King of Spades
+            h.AddCard(new Card(7, 2)); // 7 of Diamonds
+
+            Console.WriteLine("TESTING HAND CLASS");
+            Console.WriteLine("------------------");
+
+            Console.WriteLine("Showing hand. Expecting 3 cards:");
+            Console.WriteLine(h);
+
+            Console.WriteLine("Testing NumCards. Expecting 3: " + h.NumCards);
+
+            Console.WriteLine("Testing GetCard(0). Expecting Ace of Clubs: " + h.GetCard(0));
+
+            Console.WriteLine("Testing HasCard(1). Expecting True: " + h.HasCard(1));
+            Console.WriteLine("Testing HasCard(5). Expecting False: " + h.HasCard(5));
+
+            Console.WriteLine("Testing HasCard(13, 4). Expecting True: " + h.HasCard(13, 4));
+            Console.WriteLine("Testing HasCard(13, 1). Expecting False: " + h.HasCard(13, 1));
+
+            Console.WriteLine("Testing IndexOf(7). Expecting 2: " + h.IndexOf(7));
+            Console.WriteLine("Testing IndexOf(10). Expecting -1: " + h.IndexOf(10));
+
+            Console.WriteLine("Testing IndexOf(13, 4). Expecting 1: " + h.IndexOf(13, 4));
+            Console.WriteLine("Testing IndexOf(13, 1). Expecting -1: " + h.IndexOf(13, 1));
+
+            Card removed = h.Discard(0);
+
+            Console.WriteLine("Testing Discard(0). Expecting Ace of Clubs: " + removed);
+            Console.WriteLine("Testing NumCards after discard. Expecting 2: " + h.NumCards);
+
+            Console.WriteLine();
+        }
+
+        static void TestBJHand()
+        {
+            Console.WriteLine("TESTING BJHAND CLASS");
+            Console.WriteLine("--------------------");
+
+            BJHand hand1 = new BJHand(); // Tetsting Ace + King. Expecting score 21.
+            hand1.AddCard(new Card(1, 1)); // Ace of Clubs
+            hand1.AddCard(new Card(13, 4)); // King of Spades
+
+            Console.WriteLine("Testing Ace + King. Expecting score 21.");
+            Console.WriteLine(hand1);
+            Console.WriteLine("Testing HasAce. Expecting True: " + hand1.HasAce);
+            Console.WriteLine("Testing Score. Expecting 21: " + hand1.Score);
+            Console.WriteLine("Testing IsBusted. Expecting False: " + hand1.IsBusted);
+            Console.WriteLine();
+
+            BJHand hand2 = new BJHand(); // Testing Ace + 5. Expecting score 16.
+            hand2.AddCard(new Card(1, 2)); // Ace of Diamonds
+            hand2.AddCard(new Card(5, 1)); // 5 of Clubs
+
+            Console.WriteLine("Testing Ace + 5. Expecting score 16.");
+            Console.WriteLine(hand2);
+            Console.WriteLine("Testing HasAce. Expecting True: " + hand2.HasAce);
+            Console.WriteLine("Testing Score. Expecting 16: " + hand2.Score);
+            Console.WriteLine("Testing IsBusted. Expecting False: " + hand2.IsBusted);
+            Console.WriteLine();
+
+            BJHand hand3 = new BJHand(); // Testing King + Queen + 5. Expecting Bust of 25.
+            hand3.AddCard(new Card(13, 1)); // King of Clubs
+            hand3.AddCard(new Card(12, 2)); // Queen of Diamonds
+            hand3.AddCard(new Card(5, 3)); // 5 of Hearts
+
+            Console.WriteLine("Testing King + Queen + 5. Expecting score 25.");
+            Console.WriteLine(hand3);
+            Console.WriteLine("Testing HasAce. Expecting False: " + hand3.HasAce);
+            Console.WriteLine("Testing Score. Expecting 25: " + hand3.Score);
+            Console.WriteLine("Testing IsBusted. Expecting True: " + hand3.IsBusted);
+            Console.WriteLine();
+
         }
     }
 }
